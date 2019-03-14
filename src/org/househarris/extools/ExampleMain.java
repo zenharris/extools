f2bc1780d04b47fa2e561d65c36d2f0dffbf8f4c
Zen Harris <zen@b5.net>
2019-03-14 16:51:56 +1100

    Add files via upload

---
+++ src/org/househarris/extools/ExampleMain.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * @author Zen Harris
+
+                        Example Main for simple database with 2 files.
+*/
+
+
+import javax.swing.JFrame;
+import org.househarris.extools.wdbm;
+
+
+public class ExampleMain {
+
+    
+    public static void main(String[] args) {
+        
+        
+        
+        try {
+            wdbm primaryFile = new wdbm("prim.dict");
+            ///wdbm resieFile = new wdbm("resie.dict");
+            primaryFile.ActivateWDBM();
+            ///resieFile.ActivateWDBM();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+}
+   
