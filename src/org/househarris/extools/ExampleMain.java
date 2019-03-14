01ad6e1365b15678b11359428314fe52b1bd9a0c
Zen Harris <zen@b5.net>
2019-03-14 16:54:54 +1100

    Add files via upload

--- src/org/househarris/extools/ExampleMain.java
+++ src/org/househarris/extools/ExampleMain.java
@@ -19,9 +19,9 @@ public class ExampleMain {
         
         try {
             wdbm primaryFile = new wdbm("prim.dict");
-            ///wdbm resieFile = new wdbm("resie.dict");
+            wdbm resieFile = new wdbm("resie.dict");
             primaryFile.ActivateWDBM();
-            ///resieFile.ActivateWDBM();
+            resieFile.ActivateWDBM();
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
